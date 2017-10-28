25b8c105f047e01308c62e97325f33717b0a6377
Fiona <strakafiona@gmail.com>
2017-10-28 13:28:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,19 +13,3 @@ function multiply(a, b) {
 function divide(a, b) {
   return (a / b)
 }
-
-var number = 10
-
-function add5() {
-  number += 5
-}
-
-function divideBy3() {
-  number /= 3
-}
-
-divideBy3()
-console.log(number)
-
-add5()
-console.log(number)
